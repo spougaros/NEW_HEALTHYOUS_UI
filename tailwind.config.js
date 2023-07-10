089a6ee99dfcd2f5d1b832c186592a179764bc16
spougaros <56828638+spougaros@users.noreply.github.com>
2023-07-10 13:26:00 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    theme: {
+        extend: {
+            colors: {
+                dimgray: "#546a83",
+                darkslate: "#263648",
+                lightgray: "#d2dae3",
+                ghostwhite: "#f8f9fa",
+                seagreen: "#00bd9c", // success color
+                burlywood: "#edba74", // warning color
+                salmon: "#ff6d6f", // danger color
+                flowerblue: "#527ff0", // primary color
+                steelblue: "#4458b8",
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/forms")],
+};
