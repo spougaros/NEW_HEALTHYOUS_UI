e6bd8599d5b2cf3f7d85ebf796922d3a7e2705f6
spougaros <56828638+spougaros@users.noreply.github.com>
2023-07-11 18:32:50 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export { departments, gender } from "./select-options";
 // export const STATIC_FILE_BASE = "http://localhost:9003/static/";
 export const STATIC_FILE_BASE =
     process.env.NODE_ENV === "production"
-        ? "fervent-clarke.185-138-42-154.plesk.page/static/"
+        ? "https://api.masterweb.gr/static/"
         : "http://localhost:9003/static/"; 
 export {
     // Auth
